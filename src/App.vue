462cd30b7f28c3ae366ec5bafe32f42b27ca4456
fbk111 <2290291854@qq.com>
2023-07-03 23:06:24 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+* {
+  padding: 0px;
+  margin: 0px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+//引入ag-grid的样式文件，也可放main.js文件全局引用
+//引入ag-grid的样式文件，也可放main.js文件全局引用
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+// 加载 element 组件库
+import ElementUI from 'element-ui'
+
+// 加载 element 组件库的样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 全局注册 element 组件库
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
